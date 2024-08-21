10bc2ccfc95825a31de37afead2ea0cddf4f78ab
DayOS <79074357+DayOS9@users.noreply.github.com>
2024-08-21 04:27:45 -0700

    added keybind to toggle between light and dark mode

--- nvim/lua/daois/plugins/colorscheme.lua
+++ nvim/lua/daois/plugins/colorscheme.lua
@@ -2,6 +2,16 @@ return {
 	"rebelot/kanagawa.nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme kanagawa-wave")
+		vim.cmd("colorscheme kanagawa")
+
+		vim.keymap.set("n", "<leader>cc", function()
+			if vim.o.background == "dark" then
+				vim.o.background = "light"
+				vim.cmd("colorscheme kanagawa-lotus")
+			else
+				vim.o.background = "dark"
+				vim.cmd("colorscheme kanagawa-wave")
+			end
+		end, { desc = "Toggle between dark/light mode" })
 	end,
 }
